5afb91c5b57b01500ace1be26149b3a497457a86
jesset <jesse.tian@me.com>
2018-05-18 20:26:48 +0800

    enhance usb/sdcard mounts for extfat/ntfs

--- usr/local/bin/moode_tunes_jesset.sh
+++ usr/local/bin/moode_tunes_jesset.sh
@@ -10,6 +10,12 @@ export SQLDB=/var/local/www/db/moode-sqlite3.db
 export nousb_flag=/boot/NOUSB
 export noled_flag=/boot/NOLED
 
+export usb_mount=/tmp/usb_mount.lock
+export mount_opts="ro,noexec,nodev,noatime,nodiratime"
+
+export sdcard_mount=/tmp/sdcard_mount.lock
+export sdcard_mountopts="noexec,nodev,noatime,nodiratime"
+
 
 unload_eth0(){
   # Pi 3B
@@ -63,12 +69,10 @@ unload_all_usbdev(){
   fi
 }
 
-###
 
-if ! test -e /etc/ssh/ssh_host_rsa_key ;then
-  echo "Regenerating OpenSSH server Host keys ..."
-  dpkg-reconfigure openssh-server
-fi
+
+
+
 
 # Disable LED on demand ...
 if test -e $noled_flag; then
@@ -76,23 +80,63 @@ if test -e $noled_flag; then
   echo none | tee /sys/class/leds/*/trigger
 fi
 
-# sysctl ...
-echo 1 > /proc/sys/kernel/sched_nr_migrate
+# Reduce OS jitter
+echo "# adjusting rcu_sched/rcu_preempt ..."
+for i in `pgrep rcu[^c]` ; do taskset -pc 0,1 $i ; done
+echo 3 | tee /sys/bus/workqueue/devices/writeback/cpumask
+echo 1 | tee /proc/sys/kernel/sched_nr_migrate
+
+# To keep all CPUs with the same rt_runtime, disable the NO_RT_RUNTIME_SHARE logic
+# https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux_for_real_time/7/html/tuning_guide/real_time_throttling
+# echo NO_RT_RUNTIME_SHARE > /sys/kernel/debug/sched_features
+
+#echo 0 > /proc/sys/kernel/watchdog
+#echo 60 > /proc/sys/kernel/watchdog_thresh
+sysctl -w vm.stat_interval=120
+sysctl -w kernel.softlockup_all_cpu_backtrace=1
+
+
+
 
 
 
 eth0chk=$(sqlite3 $SQLDB "PRAGMA query_only=1; PRAGMA busy_timeout=5000; select value from cfg_system where param='eth0chk'" | tail -1)
  i2sdev=$(sqlite3 $SQLDB "PRAGMA query_only=1; PRAGMA busy_timeout=5000; select value from cfg_system where param='i2sdevice'" | tail -1)
 
+
+echo '# unbind Ethernet(eth0) if it is not ACTULLY in use (determined by Up/Down status, and addr)'
+for c in {60..1};do
+  echo "# Waiting for moOde fully startup for $c ."
+  if grep -q Ready /var/log/moode.log ;then
+    echo "# moOde fully ready, beginning detect eth0 ..."
+    eth0_stat=$(ip addr show dev eth0 | grep -Po 'state \S+' | cut -d ' ' -f 2)
+    eth0_addr=$(ip addr show dev eth0 | grep -Po 'inet [\d\./]+' | cut -d ' ' -f 2)
+    if [[ "${eth0_stat}" == "UP" ]]||[[ x${eth0_addr} != "x" ]];then
+      echo "# eth0 is in use, tunning..."
+      ethtool --set-eee eth0  eee off
+      break
+    else
+      unload_eth0
+      break
+    fi
+  else
+    sleep 1
+    continue
+  fi
+done
+
+
 echo "# USB tunning..."
 for (( i = 0; i < 1; i++ )); do
-  # Condition 1: Disable All USB Port, if any:
+  # Condition : Disable All USB Port, if any:
   # 1. using i2c dac && eth0 not enabled
   # 2. /boot/NOUSB touched
   if ( [[ ${eth0chk} -eq 0  ]] && [[ "${i2sdev}" != none  ]] ) || test -e $nousb_flag ; then
-    echo "# unload all usb port, eth0chk=${eth0chk}, i2sdev=${i2sdev}"
-    unload_all_usbdev
-    break
+    if ! grep -q usb_storage /proc/modules; then
+      echo "# unload all usb port, eth0chk=${eth0chk}, i2sdev=${i2sdev}"
+      unload_all_usbdev
+      break
+    fi
   fi
 
   if [[ ${eth0chk} -eq 0  ]];then
@@ -106,68 +150,19 @@ for (( i = 0; i < 1; i++ )); do
     ps -e -o pid,psr,comm,args | grep -Pi -- '-dwc_otg' | grep -v grep | awk '{print $1}' | while read pid;
     do
       taskset -p --cpu-list 0-1 $pid
-    done    
+    done
   else
     echo "# You chosed I2S DAC."
-    for port in 2 3 4 5;do
-      echo "#   Disable USB Hub port $port ..."
-      /usr/local/bin/hub-ctrl -b 1 -d 2 -P $port -p 0
-      sleep 0.3
-    done
+    if ! grep -q usb_storage /proc/modules; then
+      for port in 2 3 4 5;do
+        echo "#   Disable USB Hub port $port ..."
+        /usr/local/bin/hub-ctrl -b 1 -d 2 -P $port -p 0
+        sleep 0.3
+      done
+    fi
   fi
 done
 
-#if [[ "${i2sdev}" != none  ]];then
-#  echo "# I2S DAC used, further tunning..."
-#  echo "#   MMC0/1 (SDcard/WiFI), Adjust mmc0/1 cpu affinity/rtprio ..."
-#  echo "#   Adjust DMA irq affinity ..."
-#  pgrep 'mmc|DMA|kblockd' | while read tid;do
-#    taskset -p --cpu-list 0-1 $tid
-#    #chrt --fifo -p 33 $tid
-#  done
-#  sleep 0.3
-#  
-#  ps -q $(pgrep -d, 'mmc|DMA|kblockd')  -eL -o class,pid,lwp,psr,rtprio,pri,nice,sched,comm,args
-#
-#fi
-
-
-# Reduce OS jitter
-echo "# adjusting rcu_sched/rcu_preempt ..."
-for i in `pgrep rcu[^c]` ; do taskset -pc 0,1 $i ; done
-echo 3 | tee /sys/bus/workqueue/devices/writeback/cpumask
-
-# To keep all CPUs with the same rt_runtime, disable the NO_RT_RUNTIME_SHARE logic
-# https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux_for_real_time/7/html/tuning_guide/real_time_throttling
-# echo NO_RT_RUNTIME_SHARE > /sys/kernel/debug/sched_features
-
-#echo 0 > /proc/sys/kernel/watchdog
-#echo 60 > /proc/sys/kernel/watchdog_thresh
-sysctl -w vm.stat_interval=120
-sysctl -w kernel.softlockup_all_cpu_backtrace=1
-
-echo '# Finally, unbind Ethernet(eth0) if it is not ACTULLY in use (determined by Up/Down status, and addr)'
-if ip link show eth0 >/dev/null 2>&1 ;then
-  for c in {1..60};do
-    echo "# Waiting for moOde fully startup for $c time."
-    if grep -q Ready /var/log/moode.log ;then
-      echo "# moOde fully ready, beginning detect eth0 ..."
-      eth0_stat=$(ip addr show dev eth0 | grep -Po 'state \S+' | cut -d ' ' -f 2)
-      eth0_addr=$(ip addr show dev eth0 | grep -Po 'inet [\d\./]+' | cut -d ' ' -f 2)
-      if [[ "${eth0_stat}" == "UP" ]]||[[ x${eth0_addr} != "x" ]];then
-        echo "# eth0 is in use, tunning..."
-	ethtool --set-eee eth0  eee off
-        break
-      else
-        unload_eth0
-        break
-      fi
-    else
-      sleep 1
-      continue
-    fi
-  done
-fi
 
 if ip link show wlan0 >/dev/null 2>&1 ;then
   iwconfig wlan0 frag 512
@@ -176,16 +171,15 @@ if ip link show wlan0 >/dev/null 2>&1 ;then
 fi
 
 
+export mounted_srcs=''
 # Automatic mount USB Storage
-export usb_mount=/tmp/usb_mount.lock
-export mount_opts="ro,noexec,nodev,noatime,nodiratime"
 
 lsblk --pairs --noheadings --paths --bytes \
       --exclude 179,7 \
       --output name,size,type,rm,uuid,label,fstype | \
 while read line ;do
  eval "$line"
- # only mount removable media and 
+ # only mount removable media and
  if [[ $TYPE != "disk" ]] && [[ "$LABEL" != "EFI" ]];then
    echo "INFO: new volume to mount: type=$TYPE name=$NAME uuid=$UUID label='$LABEL'"
    if findmnt -nl --source $NAME ;then
@@ -201,27 +195,27 @@ while read line ;do
          mount_opts+=",dmask=0022,fmask=0022"
        ;;
      esac
+     echo "mount -o $mount_opts $NAME \"$target\"" > /dev/shm/mount.sh
      mount -o $mount_opts $NAME "$target" && \
        touch $usb_mount && \
        echo "INFO: mounted $NAME to '$target'"
+     mounted_srcs+=" $NAME"
    fi
  fi
  unset RM TYPE LABEL UUID
 done
 
-test -e $usb_mount && mpc update USB && rm -f $usb_mount 
+test -e $usb_mount && mpc update USB
 
 
 # Automatic mount SDcard partitions
-export sdcard_mount=/tmp/sdcard_mount.lock
-export sdcard_mountopts="noexec,nodev,noatime,nodiratime"
 
 lsblk --pairs --noheadings --paths --bytes \
       --include 179 \
       --output name,size,type,rm,uuid,label,fstype,mountpoint | \
 while read line ;do
  eval "$line"
- # only mount removable media and 
+ # only mount removable media and
  if [[ $TYPE == "part" ]] && \
 	 [[ "$LABEL" != "BOOT" ]] && \
 	 [[ "$LABEL" != "ROOTFS" ]] && \
@@ -240,16 +234,28 @@ while read line ;do
          sdcard_mountopts+=",dmask=0022,fmask=0022"
        ;;
      esac
+     echo "mount -o $sdcard_mountopts $NAME \"$target\"" >> /dev/shm/mount.sh
      mount -o $sdcard_mountopts $NAME "$target" && \
        touch $sdcard_mount && \
        echo "INFO: mounted $NAME to '$target'"
+     mounted_srcs+=" $NAME"
    fi
  fi
  unset RM TYPE LABEL UUID
 done
 
-test -e $sdcard_mount && mpc update SDCARD && rm -f $sdcard_mount 
+test -e $sdcard_mount && mpc update SDCARD
 
+echo "INFO: re-check usb/sdcard mounts"
+for c in {1..180};do
+  for src in $mounted_srcs ;do
+     if ! findmnt -nl --source $src ;then
+       echo "WARN: $src mounted failed, remount..."
+       source /dev/shm/mount.sh
+     fi
+  done
+  sleep 1
+done
 
 
 echo "Finished."
