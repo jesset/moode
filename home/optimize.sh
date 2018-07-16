49d55fc258d9e6929c9ba2787fd8b8714e720e9d
jesset <jesse.tian@me.com>
2018-07-16 10:46:55 +0800

    svcs

--- home/optimize.sh
+++ home/optimize.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
+# systemctl list-unit-files | grep enabled
+systemctl disable nmbd.service
+systemctl disable smbd.service
+systemctl disable 'getty@.service'
+systemctl disable 'autovt@.service'
+systemctl disable console-setup.service
+systemctl disable rpi-display-backlight.service
+systemctl disable keyboard-setup.service
+systemctl disable rsync.service
+systemctl disable apt-daily-upgrade.timer
+systemctl disable apt-daily.timer
+systemctl disable phpsessionclean.timer
+systemctl disable systemd-timesyncd.service
+systemctl disable sysstat.service
 systemctl disable bthelper.service
 systemctl disable syslog.service
 systemctl disable phpsessionclean.timer
+
+# 禁用 rsyslog ，只用 journald
+systemctl mask rsyslog.service
+systemctl mask rsyslogd.service
+systemctl mask syslog.service
+systemctl stop syslog.socket
+systemctl stop rsyslog.service
 
 for i in 20-exif.ini 20-ftp.ini 20-calendar.ini 20-shmop.ini 20-sysvshm.ini 20-sysvsem.ini 20-sysvmsg.ini
 do
