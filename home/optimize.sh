b33f71b768ef8fa836f1292dbe1728c6dea7969e
jesset <jesse.tian@me.com>
2018-07-16 10:04:09 +0800

    optimize.sh

---
+++ home/optimize.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+systemctl disable bthelper.service
+systemctl disable syslog.service
+systemctl disable phpsessionclean.timer
+
+for i in 20-exif.ini 20-ftp.ini 20-calendar.ini 20-shmop.ini 20-sysvshm.ini 20-sysvsem.ini 20-sysvmsg.ini
+do
+  test -e /etc/php/7.0/fpm/conf.d/$i && rm -fv  /etc/php/7.0/fpm/conf.d/$i
+done
+
